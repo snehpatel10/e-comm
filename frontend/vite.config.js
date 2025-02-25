cbf719ab03e47ecb9dbc1f18f9c7c9fea1d67798
snehpatel10 <thisissnehpatel@gmail.com>
2025-02-25 13:48:26 +0530

    bug fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,21 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
-      '/upload': 'http://localhost:5000'
+      '/upload': 'http://localhost:5000',
+    },
+  },
+  
+  optimizeDeps: {
+    include: ['react-toastify', 'framer-motion'], 
+  },
+  
+  ssr: {
+    external: ['react-toastify', 'framer-motion'], 
+  },
+  
+  build: {
+    rollupOptions: {
+      external: ['react-toastify', 'framer-motion'], 
     },
   },
 });
