8eeb2303820c084e006db45c8bef9980f011a35b
snehpatel10 <thisissnehpatel@gmail.com>
2025-03-03 15:53:29 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://e-comm-my7s.vercel.app',
-      '/upload': 'https://e-comm-my7s.vercel.app',
+      '/api': 'https://e-comm-my7s-mqu71f644-snehpatel10s-projects.vercel.app/',
+      '/upload': 'https://e-comm-my7s-mqu71f644-snehpatel10s-projects.vercel.app/',
     },
   },
 
