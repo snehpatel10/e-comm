b0423d04f449f85dfe685a3b48ba6d6258ee46bf
snehpatel10 <thisissnehpatel@gmail.com>
2025-03-03 14:57:46 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL, 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
